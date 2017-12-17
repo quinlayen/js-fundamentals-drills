1b651d1ab773db13880d071a471d47ef2d4a81a9
quinlayen <quinlayen@gmail.com>
2017-12-17 12:35:10 -1000

    stringCountBelowThree completed

--- exercises.js
+++ exercises.js
@@ -280,7 +280,15 @@ var sumAllPositive = function(num){
  * @param {Array}
  * @return {Number}
  */
-var stringCountBelowThree;
+var stringCountBelowThree = function (arr){
+  result = [];
+  for (var i = 0; i < arr.length; i++){
+    if (arr[i].length <= 3){
+      result.push(arr[i]);
+    }
+  }
+  return result.length;
+}
 
 /* #countObjects
  *
